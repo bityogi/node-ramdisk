7e4c1b02c42cb7cf673b8e3cf4a1347fa7c234d5
bityogi <bityogi@tutanota.com>
2018-05-22 13:38:10 -0400

    linux mount commands

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -4,21 +4,23 @@ set -e
 
 mk_ram_disk() {
   local ramfs_size_mb=$1
-  local mount_point=${2}
+  local mount_point=/dev/${2}
 
   if EXISTS=`mount | grep ${mount_point}`; then
     printf "device \"%s\" already mounted" ${mount_point} 1>&2
     exit 1
   fi
 
-  udisksctl mount -b /dev/${mount_point}
+  mkdir ${mount_point} 
+  chmod 777 ${mount_point} 
+  udisksctl mount -b ${mount_point}
   
   printf %s ${mount_point//[[:blank:]]/}
 }
 
 rm_ram_disk() {
-  local mount_point=${2}
-  udisksctl umount /dev/${mount_point}
+  local mount_point=/dev/${2}
+  udisksctl umount -b ${mount_point}
 }
 
 main() {
